6a8560e7d1b6c6887d3ea89e102774db37b3cec8
Win Sandy <winsandymyint@gmail.com>
2016-11-15 00:43:34 +0630

    Add '05-5' Years Old in F201

--- project/apps/birth_registration/migrations/0050_auto_20160418_2007.py
+++ project/apps/birth_registration/migrations/0050_auto_20160418_2007.py
@@ -23,6 +23,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='f201',
             name='AGE_GROUP',
-            field=birth_registration.fields.AgeGroupField(default=99, help_text='Age group for cause of death', choices=[(0, '0 - Under 1 year'), (1, '1 - 1 year'), (2, '2 - 2 years'), (3, '3 - 3 years'), (4, '4 - 4 years'), (5, '6 - 5-9 years'), (6, '7 - 10-14 years'), (7, '8 - 15-19 years'), (8, '9 - 20-24 years'), (9, '10 - 25-29 years'), (10, '11 - 30-34 years'), (11, '12 - 35-39 years'), (12, '13 - 40-44 years'), (13, '14 - 45-49 years'), (14, '15 - 50-54 years'), (15, '16 - 55-59 years'), (16, '17 - 60-64 years'), (17, '18 - 65-69 years'), (18, '19 - 70-74 years'), (19, '20 - 75-79 years'), (20, '21 - 80-84 years'), (21, '22 - 85 and over'), (99, '23 - No stated')]),
+            field=birth_registration.fields.AgeGroupField(default=99, help_text='Age group for cause of death', choices=[(0, '0 - Under 1 year'), (1, '1 - 1 year'), (2, '2 - 2 years'), (3, '3 - 3 years'), (4, '4 - 4 years'), (5, '5 - 5 years'), (6, '6 - 5-9 years'), (7, '7 - 10-14 years'), (8, '8 - 15-19 years'), (9, '9 - 20-24 years'), (10, '10 - 25-29 years'), (11, '11 - 30-34 years'), (12, '12 - 35-39 years'), (13, '13 - 40-44 years'), (14, '14 - 45-49 years'), (15, '15 - 50-54 years'), (16, '16 - 55-59 years'), (17, '17 - 60-64 years'), (18, '18 - 65-69 years'), (19, '19 - 70-74 years'), (20, '20 - 75-79 years'), (21, '21 - 80-84 years'), (22, '22 - 85 and over'), (99, '23 - No stated')]),
         ),
     ]
